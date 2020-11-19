c2a550b5a9950135a3e0ab192a31c5de6f551137
Luca-Leoni <luca.leoni.99@gmail.com>
2020-11-20 00:19:41 +0100

    Primo headers della classe Fire

---
+++ ClassiLion/Fire.hxx
@@ -0,0 +1,37 @@
+/*********************************************************************
+ * 
+ * Class for the rappresentation of the fire, it will be an extension
+ * of the class WaveFront and is going to make the calculation fro the
+ * expansion etcetera
+ * 
+ *********************************************************************/
+
+# pragma once
+
+# include "WaveFront.hxx"
+
+// For future class that is needed
+class Enviroment;
+
+struct Fire : public WaveFront
+{
+    // Pointer to the class enviroment for taking the ambiental 
+    // parameter needed for calculation
+    Enviroment * Forest;
+
+    Fire(Enviroment * Forest, double Xi, double Yi);
+    Fire(const Fire & f);
+
+    //--------------Metods----------------
+
+    // Metod for the spread of fire based Huygens principle
+    void Propagate(double dt);
+
+    // Metod for computing the a, b and c of the ellipses in a 
+    // particular vertex signed by an index i
+    double * elipsesAxises(int i);
+
+    // Metod for getting the RoS of a vertex signed by an index i 
+    // stored in the right cell of the enviroment
+    double getRoS(int i);
+};
