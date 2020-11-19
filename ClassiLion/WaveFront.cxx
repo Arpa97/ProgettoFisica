0995b74f67d28459edb44c07f683d37407573f3a
Francesco Salvi <74156537+frasalvi@users.noreply.github.com>
2020-11-19 03:00:41 +0100

    Grammatica e poco altro
    Correzione grammaticale di alcune parole, piccole modifiche qua e là in particolare a checkDistance

--- ClassiLion/WaveFront.cxx
+++ ClassiLion/WaveFront.cxx
@@ -1,29 +1,31 @@
 # include "WaveFront.hxx"
 
-WaveFront::WaveFront(double x, double y): Poligon(1) 
+WaveFront::WaveFront(double x, double y): Polygon(1) 
 {
-    Poligon[0].x = x, Poligon[0].y = y;
+    Polygon[0].x = x, Polygon[0].y = y;
 }
 
-WaveFront::WaveFront(Vertex * ver, int N): Poligon(N) 
+WaveFront::WaveFront(Vertex * ver, int N): Polygon(N) 
 {
     for(int i = 0; i != N; i++)
-    Poligon[i] = ver[i];
+    {
+        Polygon[i] = ver[i];
+    }
 }
 
-WaveFront::WaveFront(const WaveFront & wf): Poligon(wf.Poligon) {}
+WaveFront::WaveFront(const WaveFront & wf): Polygon(wf.Polygon) {}
 
 
-//--------------Metodi-----------------
+//--------------Methods-----------------
 
 
 void WaveFront::insertVertex(double x, double y, int i)
 {
     std::vector<Vertex>::iterator it;
 
-    it = i == -1 ? Poligon.end() + 1 : Poligon.begin();
+    it = (i == -1) ? (Polygon.end() + 1) : (Polygon.begin());
 
-    Poligon.insert(it + i, Vertex(x, y));
+    Polygon.insert(it + i, Vertex(x, y));
 }
 
 
@@ -31,81 +33,78 @@ void WaveFront::insertVertex(const Vertex & v, int i)
 {
     std::vector<Vertex>::iterator it;
 
-    it = i == -1 ? Poligon.end() + 1 : Poligon.begin();
+    it = (i == -1) ? (Polygon.end()) + 1 : (Polygon.begin());
 
-    Poligon.insert(it + i, v);
+    Polygon.insert(it + i, v);
 }
 
 
-void WaveFront::ceckDistance(int ii)
+void WaveFront::checkDistance()
 {
 
-    for(int i = ii; i != Poligon.size() - 1; i++)
-    if(Distanza(Poligon[i], Poligon[i + 1]) > DISTMAX)
+    for(int i = 0; i != Polygon.size() - 1; i++)
+    if(Distanza(Polygon[i], Polygon[i + 1]) > DISTMAX)
     {
         insertVertex(
             // Insert the mid point
-            (Poligon[i].x + Poligon[i + 1].x)/2,
-            (Poligon[i].y + Poligon[i + 1].y)/2,
+            (Polygon[i].x + Polygon[i + 1].x)/2,
+            (Polygon[i].y + Polygon[i + 1].y)/2,
             i + 1
-        );
+                    );
     
         // Return back to see if the 
         // mid point inserted is at a right distance
         i--;
     }
 
-    int n = Poligon.size() - 1;
+    int n = Polygon.size() - 1;
 
     // Inserting between the last segment
-    if(Distanza(Poligon[n], Poligon[0]) > DISTMAX)
+    while(Distanza(Polygon[n], Polygon[0]) > DISTMAX)
     {
         insertVertex(
             // Insert the mid point
-            (Poligon[n].x + Poligon[0].x)/2,
-            (Poligon[n].y + Poligon[0].y)/2,
+            (Polygon[n].x + Polygon[0].x)/2,
+            (Polygon[n].y + Polygon[0].y)/2,
             n + 1
-        );
-
-        ceckDistance(n - 2);
+                    );
     }
-
 }
 
 
 bool WaveFront::isColliding(const Vertex & v)
 {
     int crosNum = 0;
-    int n = Poligon.size();
+    int n = Polygon.size();
 
     for(int i = 0; i != n - 1; i++)
     {
         // If either the vertex are above, or at the left of the point skip it
         if( 
-            (Poligon[i].y > v.y && Poligon[i + 1].y > v.y) || 
-            (Poligon[i].y < v.y && Poligon[i + 1].y < v.y) ||
-            (Poligon[i].x < v.x && Poligon[i + 1].x < v.x)
-        )continue;
+            (Polygon[i].y > v.y && Polygon[i + 1].y > v.y) || 
+            (Polygon[i].y < v.y && Polygon[i + 1].y < v.y) ||
+            (Polygon[i].x < v.x && Polygon[i + 1].x < v.x)
+          )continue;
 
         // scamming false crossing
 
-        double xcross = (v.y - Poligon[i].y)*(Poligon[i].x -Poligon[i + 1].x)/(Poligon[i].y -Poligon[i + 1].y) + Poligon[i].x;
+        double xcross = (v.y - Polygon[i].y)*(Polygon[i].x - Polygon[i + 1].x)/(Polygon[i].y - Polygon[i + 1].y) + Polygon[i].x;
 
-        crosNum += xcross > v.x ? 1 : 0;
+        crosNum += (xcross > v.x) ? 1 : 0;
     }
 
-    // cecking the last segment
+    // checking the last segment
     if( 
-        !(Poligon[n - 1].y > v.y && Poligon[0].y > v.y) && 
-        !(Poligon[n - 1].y < v.y && Poligon[0].y < v.y) &&
-        !(Poligon[n - 1].x < v.x && Poligon[0].x < v.x)
-    )
+        !(Polygon[n - 1].y > v.y && Polygon[0].y > v.y) && 
+        !(Polygon[n - 1].y < v.y && Polygon[0].y < v.y) &&
+        !(Polygon[n - 1].x < v.x && Polygon[0].x < v.x)
+      )
     {
         // scamming false crossing
 
-        double xcross = (v.y - Poligon[n - 1].y)*(Poligon[n - 1].x -Poligon[0].x)/(Poligon[n - 1].y -Poligon[0].y) + Poligon[n - 1].x;
+        double xcross = (v.y - Polygon[n - 1].y)*(Polygon[n - 1].x - Polygon[0].x)/(Polygon[n - 1].y - Polygon[0].y) + Polygon[n - 1].x;
 
-        crosNum += xcross > v.x ? 1 : 0;
+        crosNum += (xcross > v.x) ? 1 : 0;
     }
 
     return crosNum % 2 == 1;
